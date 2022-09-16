f5346bec5c3040ec23c84e05704eaec78402dd27
Can Mingir <canmingir@gmail.com>
2022-09-16 13:29:38 -0400

    Forward all traffic with sandbox id

--- src/routes/sandbox.js
+++ src/routes/sandbox.js
@@ -1,9 +1,10 @@
 const router = require("express").Router();
 const { spawn } = require("child_process");
 const uuid = require("uuid").v4;
-const { post } = require("axios").default;
+const axios = require("axios").default;
 
 let port = 4000;
+const map = {};
 
 router.post("/sandbox", async (req, res) => {
   const id = uuid(); //.replaceAll("-", "");
@@ -15,12 +16,15 @@ router.post("/sandbox", async (req, res) => {
 
   nuc.stdout.on("data", async () => {
     const openapi = port++;
+    map[id] = openapi;
 
-    post(`http://localhost:${terminal}/openapi`, {
-      ...req.body,
-      action: "start",
-      port: openapi,
-    })
+    axios
+      .post(`http://localhost:${terminal}/openapi`, {
+        ...req.body,
+        action: "start",
+        port: openapi,
+        prefix: `/sandbox/${id}`,
+      })
       .then(() => res.json({ id }))
       .catch((err) => res.status(500).send(err));
   });
@@ -28,6 +32,31 @@ router.post("/sandbox", async (req, res) => {
   nuc.stderr.on("data", (data) => {
     res.status(500).send(data);
   });
+});
+
+router.all("/sandbox/:id/*", async (req, res) => {
+  const { id } = req.params;
+  const { method, body, url } = req;
+
+  const port = map[id];
+
+  if (!port) {
+    res.status(404).end();
+    return;
+  }
+
+  axios({
+    method,
+    url: `http://localhost:${port}${url}`,
+    data: body,
+  })
+    .then(({ status, headers, data }) => {
+      res.set(headers).status(status).send(data);
+    })
+    .catch((err) => {
+      const { status, headers, data } = err.response;
+      res.set(headers).status(status).send(data);
+    });
 });
 
 module.exports = router;
