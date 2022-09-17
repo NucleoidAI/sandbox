dfae6529c5513395cd42510463dc451fcce805c3
Can Mingir <canmingir@gmail.com>
2022-09-17 13:08:49 -0400

    Cache process in the map

--- src/routes/sandbox.js
+++ src/routes/sandbox.js
@@ -5,23 +5,27 @@ const axios = require("axios").default;
 const metrics = require("./metrics");
 const terminal = require("./terminal");
 
+let count = 0;
 let port = 4000;
-const map = {};
+const map = new Map();
 
 router.use("/metrics", metrics);
 router.use("/terminal", terminal);
 
 router.post("/", async (req, res) => {
-  const id = uuid(); //.replaceAll("-", "");
+  const id = uuid();
   const terminal = port++;
 
-  const nuc = spawn("nuc", ["start", "--id", id, "--port", terminal], {
+  const process = spawn("nuc", ["start", "--id", id, "--port", terminal], {
     shell: true,
   });
 
-  nuc.stdout.on("data", async () => {
+  if (count > 10) {
+    console.log("Killing");
+  }
+
+  process.stdout.on("data", async () => {
     const openapi = port++;
-    map[id] = openapi;
 
     axios
       .post(`http://localhost:${terminal}/openapi`, {
@@ -30,12 +34,20 @@ router.post("/", async (req, res) => {
         port: openapi,
         prefix: `/sandbox/${id}`,
       })
-      .then(() => res.json({ id }))
+      .then(() => {
+        count++;
+        map.set(id, { process, terminal, openapi, created: Date.now() });
+        res.json({ id });
+      })
       .catch((err) => res.status(500).send(err));
   });
 
-  nuc.stderr.on("data", (data) => {
-    res.status(500).send(data);
+  process.on("exit", (code) => {
+    if (code) {
+      res.status(500).json({
+        message: "Problem occurred during spawning",
+      });
+    }
   });
 });
 
@@ -43,16 +55,17 @@ const redirect = async (req, res) => {
   const { id } = req.params;
   const { method, body, url } = req;
 
-  const port = map[id];
+  const sandbox = map.get(id);
+  const { openapi } = sandbox;
 
-  if (!port) {
+  if (!sandbox) {
     res.status(404).end();
     return;
   }
 
   axios({
     method,
-    url: `http://localhost:${port}${url}`,
+    url: `http://localhost:${openapi}${url}`,
     data: body,
   })
     .then(({ status, headers, data }) => {
